dd32c81df4cb028f7b469efdcd8fa529a849d90f
Kevin Wu <wufan940@gmail.com>
2020-07-13 00:41:43 +1000

    add new file

---
+++ hi.py
@@ -0,0 +1,13 @@
+price = 10
+pen = 0.38
+rain = True
+name = 'kevin'
+print(price, pen, rain, name)
+name = input('请输入名字：')
+print('Hello,' , name)
+height = input('请输入身高:')
+weight = input('input your weight:')
+print('你的身高是：' , height)
+print('你的体重是：' , weight, '该减肥了')
+major = input('input your major:')
+print('your major:' , major)
